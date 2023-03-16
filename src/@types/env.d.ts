a0a5c3e0fdf8828a7e7a84e8daa4cfb5101deec9
Ramses <ramses.nys@vito.be>
2023-03-16 14:05:37 +0100

    Cleanup app version/date

--- src/@types/env.d.ts
+++ src/@types/env.d.ts
@@ -1,13 +1,14 @@
 /// <reference types="vite/client" />
 
+declare const VITE_APP_VERSION: string;
+declare const VITE_APP_RELEASE_DATE: string;
+
 interface ImportMetaEnv {
   VITE_NAME: string;
   VITE_TITLE: string;
   VITE_LANGUAGES: string;
   VITE_API: string;
   VITE_GEOSERVER: string;
-  VITE_APP_VERSION: string;
-  VITE_APP_RELEASE_DATE: string;
 }
 
 interface ImportMeta {
--- src/@types/vite-env.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="vite/client" />
-
-declare const VITE_APP_VERSION: string;
-declare const VITE_APP_RELEASE_DATE: string;
